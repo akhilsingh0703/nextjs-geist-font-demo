bbdae4bb074c8ef6e48f4320e23892b5e9ac31e5
Akhil Singh <165122148+akhilsingh0703@users.noreply.github.com>
2025-07-24 00:54:44 +0530

    Add .next/static/chunks/src_app_dashboard_payments_page_tsx_3753d39e._.js 1753298683331

---
+++ .next/static/chunks/src_app_dashboard_payments_page_tsx_3753d39e._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_dashboard_payments_page_tsx_dd4681e3._.js"
+],
+    source: "dynamic"
+});
