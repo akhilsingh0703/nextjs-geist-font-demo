0be1253dd37ffd18c7c69de08b1852776970db88
Akhil Singh <165122148+akhilsingh0703@users.noreply.github.com>
2025-07-24 00:55:04 +0530

    Add .next/static/chunks/src_app_dashboard_layout_tsx_9148afbf._.js 1753298703560

---
+++ .next/static/chunks/src_app_dashboard_layout_tsx_9148afbf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_ce3a7741._.js"
+],
+    source: "dynamic"
+});
